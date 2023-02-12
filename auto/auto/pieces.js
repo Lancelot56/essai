6be22a24f4b2e49a5645c97470633ec18f075301
Lancelot56 <gde81000@gmail.com>
2023-02-12 09:50:45 +0100

    modif structure

---
+++ auto/auto/pieces.js
@@ -0,0 +1,28 @@
+//url="https://github.com/Lancelot56/essai/blob/main/pieces-autos.json"
+url="http://localhost:8000/Desktop/test/json/pieces-auto.json"
+fetch(url) 
+
+.then(response => {
+    return response.json();
+  })
+  .then(data => {
+
+    let pieces = data;
+
+    var output="";
+
+    for (var i = 0; i < 5; i++) {
+    output += `
+  
+    <ul>
+                <li>Titre: ${ pieces[i].nom}</li>
+                <li>Année: ${ pieces.results[i].Description}</li>
+                <li>Genre(s): ${ pieces.results[i].Disponibilite}</li>
+                <li>Votes: ${ pieces.results[i].Prix}</li>
+                <li>Score IMDB: ${ pieces.results[i].Categories}</li>
+                <div><img src</div>
+            </ul>
+        `;
+    };
+    document.getElementById("pieces").innerHTML = output;
+  })
